babed4c63273492842d52c2813ff23f5069d99b4
Robomar <maryammomomalik@gmail.com>
2024-10-05 18:23:44 +0500

    marychange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import Homepage from './Components/Homepage';
 import './Components/Homepage.css';
-import AppDescription from './Components/AppDescription';
 import './Components/AppDescription.css';
 import Home from './Pages/Home';
+import Signup from './Components/Signup';
+import Userpage from './Pages/Userpage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
-     <Home/>
-     
+      {/* Use BrowserRouter instead of Router */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/userpage" element={<Userpage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
