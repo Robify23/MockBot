9af844d0553cae029503a514dcb648d04e60303a
unknown <musabbihafatima2@gmail.com>
2024-10-05 04:24:18 -0700

    this is my fyp

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Homepage from '../Components/Homepage';
+import AppDescription from '../Components/AppDescription';
+function Home() {
+    return (
+        <div className="App">
+            <Homepage/>
+            <AppDescription/>  
+          
+        </div>
+    )
+    
+}
+ export default Home;
